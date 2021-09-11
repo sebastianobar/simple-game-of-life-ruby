e5433aa924214ec1196981b09aae56b2e57485fb
Sebastiano Barravecchia <sebastiano.barravecchia@gmail.com>
2021-09-11 15:55:30 +0200

    feat: ask if keep running after first generation view

--- app.rb
+++ app.rb
@@ -2,9 +2,20 @@ require_relative "classes/gol/gol"
 
 gol = GameOfLife.new();
 
-while !gol.current_gen_same_as_prev do
-  sleep(1);
+2.times do
   gol.print
   gol.next_gen
+end
+
+puts "Keep running? (y,n)"
+
+input = gets
+
+if "y" == input.chomp.downcase
+  while !gol.current_gen_same_as_prev do
+    sleep(1);
+    gol.print
+    gol.next_gen
+  end
 end
 
